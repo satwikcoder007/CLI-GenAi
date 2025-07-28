8af17d1984677509e7c22c89a6c9ed1fb3a14dca
Ryzen_10 <115201346+Dipu010@users.noreply.github.com>
2025-07-28 23:05:32 +0530

    Modified the structure of Output.

--- utils/structured_output.py
+++ utils/structured_output.py
@@ -3,8 +3,12 @@ from typing import List, Optional
 
 class FunctionAnalysis(BaseModel):
     function_name: str = Field(description="Name of the function or method(Only one function or method).")
-    code: Optional[str] = Field(default=None, description="Corrected function code, required for fix tasks ")
-    description: Optional[str] = Field(default=None, description="Explanation of the function, required for description tasks not required in fixing type of task.")
+
+    code: Optional[str] = Field(default=None, 
+                                description="Corrected function of code, required for fixing tasks and for descriptive tasks")
+    
+    description: Optional[str] = Field(default=None, 
+                                       description="Explanation of the function, required for description tasks and for explaining the mistake in fixing code.")
 
 class StructuredResponse(BaseModel):
     functions: List[FunctionAnalysis] = Field(
